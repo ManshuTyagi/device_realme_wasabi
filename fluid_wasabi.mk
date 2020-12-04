314b982f947b8ab15f2ce10e7b2857b4178cb458
Himanshu Tyagi <74397342+ManshuTyagi@users.noreply.github.com>
2020-12-04 23:29:51 +0530

    Update fluid_wasabi.mk

--- fluid_wasabi.mk
+++ fluid_wasabi.mk
@@ -21,13 +21,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from wasabi device makefile
 $(call inherit-product, device/realme/wasabi/device.mk)
 
-# Inherit some common Evolution X stuff.
+
+# Device identifier. This must come after all inclusions.
 CUSTOM_BUILD_TYPE := UNOFFICIAL
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_STOCK_ARCORE := true
 TARGET_INCLUDE_WIFI_EXT := true
-WHO_TO_BLAME := ManshuTyagi
 $(call inherit-product, vendor/fluid/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
